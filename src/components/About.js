60048cef381b605f44184a08983ca819cb2210d4
Kamil Chomiak <kchomiak@outlook.com>
2023-09-05 19:00:03 -0400

    add text to about

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import { ParallaxBanner, ParallaxBannerLayer } from "react-scroll-parallax";
 import "../stylesheets/about.css";
 
 //IMAGES
-import test from "../images/about/test.png";
+import line from "../images/about/line.png";
+import developerText from "../images/about/developer-text.png";
 
 export default function About() {
   return (
@@ -11,9 +12,23 @@ export default function About() {
       <div className="aboutParallaxContainer">
         <ParallaxBanner className="aboutPB">
           <ParallaxBannerLayer
-            className="aboutTransitionPBL"
-            image={test}
+            className="aboutDevTextPBL"
+            image={developerText}
+            translateY={[15, 25]}
           ></ParallaxBannerLayer>
+          <ParallaxBannerLayer
+            className="aboutTransitionPBL"
+            image={line}
+          ></ParallaxBannerLayer>
+          <ParallaxBannerLayer className="aboutSeTextPBL">
+            <h1>SOFTWARE ENGINEER</h1>
+          </ParallaxBannerLayer>
+          <ParallaxBannerLayer className="aboutBodyTextPBL">
+            <h1>BASED IN SOUTH FLORIDA</h1>
+            <p>HTML - CSS - JS - REACT - JAVA - PYTHON</p>
+            <p>SQL - AWS - DOCKER - KUBERNETES - JENKINS</p>
+            <p>COBOL - JCL - DB2</p>
+          </ParallaxBannerLayer>
         </ParallaxBanner>
       </div>
     </div>
