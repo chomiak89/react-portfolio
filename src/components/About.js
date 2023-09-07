f16e0725fa43569e8a1457d6d4ce620cb5655795
Kamil Chomiak <kchomiak@outlook.com>
2023-09-07 10:41:25 -0400

    adjust styles

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,32 @@ import { ParallaxBanner, ParallaxBannerLayer } from "react-scroll-parallax";
 import "../stylesheets/about.css";
 
 //IMAGES
-import line from "../images/about/line.png";
 import developerText from "../images/about/developer-text.png";
 import laptop from "../images/about/laptop.png";
 
 export default function About() {
   return (
     <div className="aboutMainContainer">
-      <div className="topline"></div>
-      <div className="test"></div>
+      <div className="about-header-container">
+        <div className="topline">
+          <div className="topline-text">
+            <h1>SOFTWARE ENGINEER</h1>
+          </div>
+        </div>
+        <div className="corner-divider"></div>
+        <div className="about-subheader">
+          <div className="about-subheader-text">
+            <h1>BASED IN SOUTH FLORIDA</h1>
+          </div>
+        </div>
+        <div className="about-body">
+          <div className="about-body-text">
+            <p>HTML - CSS - JS - REACT - JAVA - PYTHON</p>
+            <p>SQL - AWS - DOCKER - KUBERNETES - JENKINS</p>
+            <p>COBOL - JCL - DB2</p>
+          </div>
+        </div>
+      </div>
       <div className="aboutParallaxContainer">
         <ParallaxBanner className="aboutPB">
           <ParallaxBannerLayer
@@ -20,23 +37,10 @@ export default function About() {
             translateY={[10, 25]}
             shouldAlwaysCompleteAnimation="true"
           ></ParallaxBannerLayer>
-          {/* <ParallaxBannerLayer
-            className="aboutTransitionPBL"
-            image={line}
-          ></ParallaxBannerLayer> */}
           <ParallaxBannerLayer
             className="aboutLaptopPBL"
             image={laptop}
           ></ParallaxBannerLayer>
-          <ParallaxBannerLayer className="aboutSeTextPBL">
-            <h1>SOFTWARE ENGINEER</h1>
-          </ParallaxBannerLayer>
-          <ParallaxBannerLayer className="aboutBodyTextPBL">
-            <h1>BASED IN SOUTH FLORIDA</h1>
-            <p>HTML - CSS - JS - REACT - JAVA - PYTHON</p>
-            <p>SQL - AWS - DOCKER - KUBERNETES - JENKINS</p>
-            <p>COBOL - JCL - DB2</p>
-          </ParallaxBannerLayer>
         </ParallaxBanner>
       </div>
     </div>
